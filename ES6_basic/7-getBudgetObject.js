ad59f35a883bef39742d4b82a42dbf1a6ffff8b0
Weebaay <jeanpaul.dijeont@gmail.com>
2024-11-14 11:21:16 +0100

    object property

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,6 @@
+// 7-getBudgetObject
 export default function getBudgetObject(income, gdp, capita) {
-	const budget = {income, gdp, capita,};
+    const budget = {income, gdp, capita,};
   
-	return budget;
-  }
+    return budget;
+}
