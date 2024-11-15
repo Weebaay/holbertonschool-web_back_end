cee41369b90f238a0f495b020423e0e5127e5c42
Weebaay <jeanpaul.dijeont@gmail.com>
2024-11-15 17:49:57 +0100

    building and pricing

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+// 5-building
+
+export default class Building {
+  // Constructeur acceptant la superficie (sqft)
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
