1c9609ea1ce3160c3f4b702158a15cab3b838dbf
Weebaay <jeanpaul.dijeont@gmail.com>
2024-11-16 09:40:39 +0100

    class named building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,16 @@
 // 5-building
 
-class Building {
-    constructor(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
-      }
-      this._sqft = sqft;
-  
-      if (this.evacuationWarningMessage === undefined) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
+export default class Building {
+  // Constructeur acceptant la superficie (sqft)
+  constructor(sqft) {
+    this._sqft = sqft;
   }
-  
-  export default Building;
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
