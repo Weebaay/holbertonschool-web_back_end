7023cf2fe58a89f71419e390cb914c097b0d2e06
Weebaay <jeanpaul.dijeont@gmail.com>
2024-12-28 13:24:26 +0100

    project of variable annotation

---
+++ python_variable_annotation/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module contenant la fonction concat avec annotations de type.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères et renvoie le résultat.
+
+    Args:
+        str1 (str): La première chaîne de caractères.
+        str2 (str): La seconde chaîne de caractères.
+
+    Returns:
+        str: La chaîne résultante de la concaténation de str1 et str2.
+    """
+    return str1 + str2
