a7f129af93372d1a14ba760f648204cbbde6abe4
Weebaay <jeanpaul.dijeont@gmail.com>
2025-01-21 21:22:00 +0100

    added fucntion create simple and more server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,12 @@
+const { appendFile } = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 });
 
 server.listen(1245);
+
+module.exports = app;
